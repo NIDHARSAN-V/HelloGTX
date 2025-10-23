44c24034b5ffce8266c0fb20f2989f5f1254081a
NIDHARSAN-V <nidharsanv.22cse@kongu.edu>
2025-10-23 23:44:41 +0530

    app.jsx organised successfully  --- next=>customer side hotel , flight , package creation and then connecting with lead

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -111,52 +111,50 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { checkAuth } from "./Store/AuthSlice";
+
 import Header from "./components/Header";
-import Home from "./pages/Home";
 import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
 import DestinationsPage from "./components/DestinationsPage";
 import HotelsPage from "./pages/hotels/HotelsPage";
 import HotelDetailPage from "./pages/hotels/HotelDetailPage";
 import FlightsPage from "./pages/flights/FlightsPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import PackagesPage from "./pages/pakages/PackagesPage";
-import ItineraryForm from "./pages/ItineraryForm/ItineraryForm";
 import PackageDetailsPage from "./pages/pakages/PackageDetailsPage";
+import CustomizePackageForm from "./pages/pakages/CustomizePackageForm";
+import ItineraryForm from "./pages/ItineraryForm/ItineraryForm";
+
 import AuthLogin from "./pages/auth/AuthLogin";
 import AuthRegister from "./pages/auth/AuthRegister";
-import { checkAuth } from "./Store/AuthSlice";
-import { useDispatch, useSelector } from "react-redux";
 import CheckAuth from "./Common/CheckAuth";
 import NotFound from "./Common/NotFound";
-import CustomizePackageForm from "./pages/pakages/CustomizePackageForm";
+
 import UserManagement from "./pages/admin/UserManagement";
+
 import HotelPackageList from "./pages/hotels/HotelPackageList";
 import HotelPackageForm from "./pages/hotels/HotelPackageForm";
 import HotelPackageDetail from "./pages/hotels/HotelPackageDetail";
+
 import FlightPackageList from "./pages/flights/FlightPackageList";
 import FlightPackageForm from "./pages/flights/FlightPackageForm";
 import FlightPackageDetail from "./pages/flights/FlightPackageDetail";
+
 import Lead from "./pages/lead/Lead";
 import LeadCreation from "./pages/lead/LeadCreation";
 import NewQuery from "./pages/lead/Query/NewQuery";
 import LeadDashboard from "./pages/lead/LeadDashBoard/LeadDashboard";
 import EditQuery from "./pages/lead/Query/EditQuery";
 
-
 const RootLayout = () => {
-
-
   const { isAuthenticated, isLoading, user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
-  // console.log(user)
-
-  //user contains role - > customer or employee
-
   useEffect(() => {
-
     dispatch(checkAuth());
   }, [dispatch]);
 
@@ -170,27 +168,41 @@ const RootLayout = () => {
 
   return (
     <Router>
-
-
-
       <div className="flex min-h-screen flex-col">
         <Header />
-
-
-
         <main className="flex-1">
           <Routes>
-            {/* Public routes that should only be accessible when not authenticated */}
 
-            {/* <Route path="/admin" element={<UserManagement />} /> */}
 
+
+
+
+            {/* ==================== PUBLIC ROUTES ==================== */}
+            <Route path="/" element={<Home />} />
+            <Route path="/destinations" element={<DestinationsPage />} />
+            <Route path="/hotels" element={<HotelsPage />} />
+            <Route path="/hotels/:id" element={<HotelDetailPage />} />
+            <Route path="/flights" element={<FlightsPage />} />
+            <Route path="/packages" element={<PackagesPage />} />
+            <Route path="/packages/:id" element={<PackageDetailsPage />} />
+            <Route path="/customize-packages" element={<CustomizePackageForm />} />
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* Auth routes - accessible only when NOT authenticated */}
             <Route
               path="/login"
               element={
-                <CheckAuth
-                  isAuthenticated={isAuthenticated}
-                  requireAuth={false}
-                >
+                <CheckAuth isAuthenticated={isAuthenticated} requireAuth={false}>
                   <AuthLogin />
                 </CheckAuth>
               }
@@ -198,10 +210,7 @@ const RootLayout = () => {
             <Route
               path="/register"
               element={
-                <CheckAuth
-                  isAuthenticated={isAuthenticated}
-                  requireAuth={false}
-                >
+                <CheckAuth isAuthenticated={isAuthenticated} requireAuth={false}>
                   <AuthRegister />
                 </CheckAuth>
               }
@@ -213,37 +222,6 @@ const RootLayout = () => {
 
 
 
-            {/* //only if user.role===employee */}
-            <Route path="/hotel-packages" element={<HotelPackageList />} />
-
-
-            <Route
-              path="/admin/hotel-packages/create"
-              element={<HotelPackageForm />}
-            />
-
-            <Route
-              path="/hotel-packages/details"
-              element={<HotelPackageDetail />}
-            />
-
-
-
-
-
-
- {/* //only if user.role===employee */}
-            <Route path="/flight-packages" element={<FlightPackageList />} />
-
-            <Route
-              path="/admin/flight-packages/create"
-              element={<FlightPackageForm />}
-            />
-
-            <Route
-              path="/flight-packages/details"
-              element={<FlightPackageDetail />}
-            />
 
 
 
@@ -253,81 +231,65 @@ const RootLayout = () => {
 
 
 
-            {/* Public routes accessible to all */}
-            <Route path="/" element={<Home />} />
-            <Route path="/destinations" element={<DestinationsPage />} />
-            <Route path="/hotels" element={<HotelsPage />} />
-            <Route path="/hotels/:id" element={<HotelDetailPage />} />
-            <Route path="/packages/:id" element={<PackageDetailsPage />} />
-            <Route
-              path="/customize-packages"
-              element={<CustomizePackageForm />}
-            />
+            {/* ==================== PROTECTED ROUTES ==================== */}
 
-            <Route path="/flights" element={<FlightsPage />} />
-            <Route path="/packages" element={<PackagesPage />} />
-
-
-
-
-
-
-
-{user && user.role=== "employee" && (
-  <>
-
-
-            <Route path="/leads/query" element={<NewQuery />} />
-
-            <Route path="/query/edit-query" element={<EditQuery/>} />
-
-            <Route path="/lead/dashboard" element={<LeadDashboard />} />
-
-            
-            <Route path="/lead" element={<Lead/>}/>
-            <Route path="/leads/create" element={<LeadCreation/>}/>
-
-</>)}
+            {/* Customer only */}
+            {user && user.role === "customer" && (
+              <>
+                <Route
+                  path="/profile"
+                  element={
+                    <CheckAuth isAuthenticated={isAuthenticated} requireAuth={true}>
+                      <ProfilePage />
+                    </CheckAuth>
+                  }
+                />
+              </>
+            )}
 
 
 
 
 
 
+            {/* Employee only */}
+            {user && user.role === "employee" && (
+              <>
+                {/* Admin / Employee management */}
+                <Route path="/admin/users" element={<UserManagement />} />
+
+                {/* Hotel package management */}
+                <Route path="/hotel-packages" element={<HotelPackageList />} />
+                <Route path="/admin/hotel-packages/create" element={<HotelPackageForm />} />
+                <Route path="/hotel-packages/details" element={<HotelPackageDetail />} />
+
+                {/* Flight package management */}
+                <Route path="/flight-packages" element={<FlightPackageList />} />
+                <Route path="/admin/flight-packages/create" element={<FlightPackageForm />} />
+                <Route path="/flight-packages/details" element={<FlightPackageDetail />} />
+
+                {/* Lead management */}
+                <Route path="/lead" element={<Lead />} />
+                <Route path="/lead/dashboard" element={<LeadDashboard />} />
+                <Route path="/leads/create" element={<LeadCreation />} />
+                <Route path="/leads/query" element={<NewQuery />} />
+                <Route path="/query/edit-query" element={<EditQuery />} />
+
+                {/* Itinerary creation */}
+                <Route
+                  path="/itenaryCreation"
+                  element={
+                    <CheckAuth isAuthenticated={isAuthenticated} requireAuth={true}>
+                      <ItineraryForm />
+                    </CheckAuth>
+                  }
+                />
+              </>
+            )}
 
 
 
 
-
-
-
-
-
-
-
-            {/* //only if user.role===customer */}
-            {/* Protected routes - only accessible when authenticated */}
-            <Route
-              path="/profile"
-              element={
-                <CheckAuth isAuthenticated={isAuthenticated} requireAuth={true}>
-                  <ProfilePage />
-                </CheckAuth>
-              }
-            />
-
-
-
-
-             {/* //only if user.role===employee */}
-            <Route
-              path="/itenaryCreation"
-              element={
-                <CheckAuth isAuthenticated={isAuthenticated} requireAuth={true}>
-                  <ItineraryForm />
-                </CheckAuth>
-              }
-            />
 
 
 
@@ -336,9 +298,8 @@ const RootLayout = () => {
 
             
 
-
-            {/* Fallback route */}
-            <Route path="*" element={<NotFound/>} />
+            {/* ==================== FALLBACK ==================== */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer />
@@ -348,3 +309,4 @@ const RootLayout = () => {
 };
 
 export default RootLayout;
+
